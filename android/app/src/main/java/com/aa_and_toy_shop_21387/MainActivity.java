b731c99fe284579806fab748c917d02bd64d897b
Crowdbotics <team@crowdbotics.com>
2020-10-12 02:18:19 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/aa_and_toy_shop_21387/MainActivity.java
@@ -0,0 +1,15 @@
+package com.aa_and_toy_shop_21387;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "aa_and_toy_shop_21387";
+    }
+}
